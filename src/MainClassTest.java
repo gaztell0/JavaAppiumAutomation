55eaf2333010978383317e58da8b37f074c1babd
Taras Shcherbak <scherbakbox@yandex.ru>
2020-08-06 19:09:54 +0300

    Добавлен тест testGetClassNumber, который проверяет, что метод getClassNumber возвращает число больше 45

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,7 +6,15 @@ public class MainClassTest extends MainClass {
     @Test
     public void testGetLocalNumber() {
         int actual = this.getLocalNumber();
-        int extend = 14;
-        Assert.assertTrue("Метод getLocalNumber возвращает " + this.getLocalNumber() + ", а не 14.",actual == extend);
+        int expected = 14;
+        Assert.assertTrue("Метод getLocalNumber возвращает " + this.getLocalNumber() + ", а не 14.",actual == expected);
     }
+
+    @Test
+    public void  testGetClassNumber() {
+        int class_number = this.getClassNumber();
+        System.out.println(class_number);
+        Assert.assertTrue("Метод getClassNumber возвращает число которое не больше 45",class_number > 45);
+    }
+
 }
