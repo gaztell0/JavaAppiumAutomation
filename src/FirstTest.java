4ae8520737c76c6b44cea7c6a8c5b980b37a691e
Taras Shcherbak <scherbakbox@yandex.ru>
2020-09-01 12:45:12 +0300

    Добавлен поворот экрана в портретный режим перед началом каждого теста

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -31,6 +31,7 @@ public class FirstTest {
         capabilities.setCapability("app","/Users/taras/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.rotate(ScreenOrientation.PORTRAIT);
     }
 
     @Test
