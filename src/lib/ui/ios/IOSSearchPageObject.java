c1ea19bbe0cd323ef261f7381936fa2896e91f6a
Taras Shcherbak <scherbakbox@yandex.ru>
2020-09-22 14:45:18 +0300

    Рефакторинг тестов на поиск

---
+++ src/lib/ui/ios/IOSSearchPageObject.java
@@ -0,0 +1,18 @@
+package lib.ui.ios;
+
+import io.appium.java_client.AppiumDriver;
+import lib.ui.SearchPageObject;
+
+public class IOSSearchPageObject extends SearchPageObject {
+    static {
+        SEARCH_INIT_ELEMENT = "xpath://XCUIElementTypeSearchField[@name='Search Wikipedia']";
+        SEARCH_INPUT = "xpath://XCUIElementTypeSearchField[@name='Search Wikipedia']";
+        SEARCH_CANCEL_BUTTON = "xpath://XCUIElementTypeStaticText[@name='Cancel']";
+        SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://XCUIElementTypeLink[contains(@name,'{SUBSTRING}')]";
+        SEARCH_RESULT_ELEMENT = "xpath://XCUIElementTypeLink";
+        EMPTY_SEARCH_RESULT = "xpath://XCUIElementTypeStaticText[@name=No results found]";
+    }
+    public IOSSearchPageObject(AppiumDriver driver) {
+        super(driver);
+    }
+}
